4b181f6458a8fbdd39769c5e05f4e503793ca770
Amit Ranan <ranan.amit@gmail.com>
2015-08-03 19:35:36 -0700

    first jquery commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    
+	$("button").click(function(e) { 
+		e.preventDefault();
+		$("ul").prepend("<li class='new-item'>" + $("input:first").val() + "<i class='fa fa-times'>" + "</i>" + "<i class='fa fa-check-circle fa-lg'>" + "</i>" + "</li>");
+		$(".fa-times, .fa-check-circle").hide();
+		$("input").val("");
+	});
+
+	$("ul").on("mouseover", "li.new-item", function() {
+		$(this).children().show();
+	});
+	$("ul").on("mouseleave", "li.new-item", function() {
+		$(this).children().hide();
+	});
+
+	// $("li").on("mouseover", function() {
+	// 	$(".fa-times, .fa-check-circle").show();
+	// })
+	// .mouseleave(function() {
+	// 	$(".fa-times, .fa-check-circle").hide();
+	// });
+
+
+	// $("li.new-item").mouseover(function() {    why does this one not work and the one above does?
+	// 	$("span.icon-container").show();
+	// })
+	// .mouseleave(function() {
+	// 	$("span.icon-container").hide();
+	// });
+
+	$(".fa-check-circle").mousedown(function() {
+		alert("hey");
+	});
+
+
+
+
+
+
+});
